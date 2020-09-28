33ebe4683635999843fba2d9cf4b9ba449ceeef2
Maksym Vitkovskyi <clevercattv@gmail.com>
2020-09-28 14:12:46 +0300

    Add swagger

--- src/main/java/com/clevercattv/top/book/config/MvcConfig.java
+++ src/main/java/com/clevercattv/top/book/config/MvcConfig.java
@@ -1,16 +1,45 @@
 package com.clevercattv.top.book.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Configuration
 @EnableWebMvc
+@EnableSwagger2
 @ComponentScan({"com.clevercattv.top.book.controller", "com.clevercattv.top.book.service",
         "com.clevercattv.top.book.client", "com.clevercattv.top.book.exception"})
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
 
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .directModelSubstitute(LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(LocalDateTime.class, java.util.Date.class)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 
 }
