8f347040f15fa493eaca682834dd2b58aa9e2a2a
Maksym Vitkovskyi <clevercattv@gmail.com>
2020-09-30 23:26:06 +0300

    Add junit-platform-runner, spring-test and remove snakeyaml dependencies. Add @Overrides to DispatcherServlet methods

--- src/main/java/com/clevercattv/top/book/config/DispatcherServlet.java
+++ src/main/java/com/clevercattv/top/book/config/DispatcherServlet.java
@@ -4,14 +4,17 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 public class DispatcherServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+    @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[]{JacksonConfig.class, DatabaseConfig.class};
     }
 
+    @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class[]{MvcConfig.class};
     }
 
+    @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
